515e0124e4ea4fb3eb4138f6fb7157efea746ffc
Gtraversa <gianlu1027@gmail.com>
2021-07-02 08:08:39 +0100

    Markdown syntax fix

--- magic_tool.py
+++ magic_tool.py
@@ -13,6 +13,7 @@ def converter(path):
                         conv.write(row[0]+ '\n')
                     elif line_count == 2:
                         conv.write(s.join(row)+ '\n')
+                        conv.write('---------------|----------'+'\n')
                     else:
                         conv.write(s.join(row)+ s+'\n')
                     line_count+= 1
